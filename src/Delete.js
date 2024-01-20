b456c4444a29057616ba330603a538d692816b12
Bakhodur Naziev <bakhodur_nazriev@ummalife.com>
2024-01-21 00:28:31 +0500

    Implement Delete page and functionality

---
+++ src/Delete.js
@@ -0,0 +1,9 @@
+const Delete = () => {
+  return (
+    <div className="blog-delete">
+
+    </div>
+  );
+}
+
+export default Delete;
---
+++ src/Edit.js
@@ -0,0 +1,9 @@
+const Edit = () => {
+  return (
+    <div className="edit-blog">
+
+    </div>
+  );
+}
+
+export default Edit;
